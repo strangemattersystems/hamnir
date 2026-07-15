29f3be05b6c3812066cd334da04e5c8e58f46b2a
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2026-07-16 00:22:00 +0100

    chore: scaffold module and CLI skeleton

---
+++ cmd/hamnir/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var version = "0.0.0-dev"
+
+func main() {
+	if len(os.Args) >= 2 && os.Args[1] == "--version" {
+		fmt.Println("hamnir", version)
+		return
+	}
+	if len(os.Args) < 2 || os.Args[1] != "serve" {
+		fmt.Fprintln(os.Stderr, "usage: hamnir serve [--config path] [--addr host:port] [--key-file path]")
+		os.Exit(2)
+	}
+
+	fs := flag.NewFlagSet("serve", flag.ExitOnError)
+	configPath := fs.String("config", "./hamnir.yaml", "path to config file")
+	addr := fs.String("addr", "127.0.0.1:5556", "listen address")
+	keyFile := fs.String("key-file", "./.hamnir/key.pem", "signing key path")
+	_ = fs.Parse(os.Args[2:])
+
+	_ = configPath
+	_ = keyFile
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintln(w, "hamnir")
+	})
+	log.Printf("hamnir listening on %s", *addr)
+	log.Fatal(http.ListenAndServe(*addr, mux))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/strangemattersystems/hamnir
+
+go 1.26.4
